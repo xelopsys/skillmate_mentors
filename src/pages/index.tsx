ea30b757c4e9defb4e21799b904787e3225bc28a
Jamshid Jurakulov <juraqulovjamshid7@gmail.com>
2023-01-17 14:31:29 +0500

    fix: errors fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from "next/head";
 import Card from "@components/card/card";
+// import { Inter } from "@next/font/google";
 import HomeScreen from "@/app/home/home-screen";
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+	const handleGoToRoute = (user: string) => {
+		const link = document.createElement("a")
+		link.href = `https://calendly.com/${user}`
+		link.target = "_blank"
+		link.click()
 
+	}
 	return (
 		<div>
 			<Head>
@@ -13,7 +21,40 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 
 			</Head>
-			<HomeScreen />
+
+			<main className="w-screen h-screen flex flex-col justify-start items-center box-border pt-24">
+				<div className="w-full h-max flex flex-col justify-center items-center gap-y-3 px-4">
+					<h1 className=" text-4xl md:text-6xl lg:text-6xl font-bold text-center">
+						Meeting with mentors
+					</h1>
+					<p className="text-lg font-normal text-gray-700 text-center mb-4 md:m-0 lg:m-0">
+						Some kind of tagline here
+					</p>
+				</div>
+				<div className="w-full h-max flex flex-row justify-center items-center gap-16 flex-wrap flex-1 mt-4">
+					<Card
+						onClick={() => handleGoToRoute("xelopsys")}
+						title="Xelopsys"
+						extraCss="md:order-2"
+						img="https://images.unsplash.com/photo-1673868077539-9c3120f78420?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+						btnText="Click"
+					/>
+					<Card
+						onClick={() => handleGoToRoute("uwu")}
+						title="UwU"
+						extraCss="lg:[&:nth-child(2)]:mb-24 order-1 md:order-2 lg:order-2"
+						img="https://images.unsplash.com/photo-1673868077539-9c3120f78420?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+						btnText="Click"
+					/>
+					<Card
+						onClick={() => { }}
+						extraCss="md:order-3"
+						title="teacher"
+						img="https://images.unsplash.com/photo-1673868077539-9c3120f78420?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+						btnText="Click"
+					/>
+				</div>
+			</main>
 		</div>
 	)
 }
